5cf6c2a8cfa79b9c22f9799a574b12dc180093cc
Sebastian Schwarzmann <sebi.schwarzmann@googlemail.com>
2022-05-15 11:21:13 +0200

    refactored compiling funtionality for solidity contracts.
    Co-authored-by: phildree <phildree@users.noreply.github.com>
    Co-authored-by: Johannes <schlingling@users.noreply.github.com>

--- packages/nft-playbook/src/app/backend/deploy_contract.ts
+++ packages/nft-playbook/src/app/backend/deploy_contract.ts
@@ -1,102 +1,18 @@
 import { ethers } from 'ethers';
-import path = require('path');
-import fs = require('fs');
-import solc = require('solc');
-
-// returns a contract object compiled using solc
-// baseContractPath: relative path of the base contract, i.e. "./BaseContract.sol"
-const instantiateContract = (baseContractPath) => {
-  const sources = {};
-  compileImports(baseContractPath, sources);
-
-  const input = {
-    language: 'Solidity',
-    sources: sources,
-    settings: {
-      outputSelection: {
-        '*': {
-          '*': ['*'],
-        },
-      },
-    },
-  };
-
-  const output = JSON.parse(solc.compile(JSON.stringify(input)));
-
-  const funcJson =
-    output['contracts']['./examples/ERC721PresetMinterPauserAutoId.sol'][
-      'ERC721PresetMinterPauserAutoId'
-    ];
-
-  return {
-    bytecode: funcJson['evm']['bytecode']['object'],
-    abi: funcJson['abi'],
-  };
-};
-
-// returns sources: { "Contract.sol": { content: fs.readFileSync("pathName.sol",utf8)...}}
-// using recursion
-const compileImports = (root, sources) => {
-  root = root.replace('"', ''); // TODO fix this!!!
-
-  sources[root] = { content: fs.readFileSync(root, 'utf8') };
-  const imports = getNeededImports(root);
-  for (let i = 0; i < imports.length; i++) {
-    compileImports(imports[i], sources);
-  }
-};
-
-// returns all the import paths in absolute path
-const getNeededImports = (path) => {
-  const file = fs.readFileSync(path, 'utf8');
-  const files = [];
-  file
-    .toString()
-    .split('\n')
-    .forEach(function (line, index, arr) {
-      if (
-        (index === arr.length - 1 && line === '') ||
-        !line.trim().startsWith('import')
-      ) {
-        return;
-      }
-      // the import is legit
-      const relativePath = line.substring(8, line.length - 2);
-      const fullPath = buildFullPath(path, relativePath);
-      files.push(fullPath);
-    });
-  return files;
-};
-
-// parent: node_modules/.../ERC721/ERC721.sol
-// returns absolute path of a relative one using the parent path
-const buildFullPath = (parent, path) => {
-  let curDir = parent.substr(0, parent.lastIndexOf('/')); //i.e. ./node/.../ERC721
-  if (path.startsWith('./')) {
-    return curDir + '/' + path.substr(2);
-  }
-
-  while (path.startsWith('../')) {
-    curDir = curDir.substr(0, curDir.lastIndexOf('/'));
-    path = path.substr(3);
-  }
-
-  return curDir + '/' + path;
-};
+import { compile_contract } from './compile_contract';
 
 export const deploy_contract = async () => {
   // get ABI and contract byte code
-  const contractInfo = instantiateContract(
-    './examples/ERC721PresetMinterPauserAutoId.sol'
+  const contractInfo = compile_contract(
+    './packages/nft-playbook/src/app/backend/contracts/ERC721PresetMinterPauserAutoId.sol'
   );
   console.log(contractInfo['abi']);
-  //console.log(contractInfo["bytecode"]);
-
+  
   const provider = ethers.providers.getDefaultProvider('http://127.0.0.1:7545');
 
   // Use your wallet's private key to deploy the contract, private key of content owner
   const privateKey =
-    '8f1851169fd5bb25e52737199d23b031c1b08f4d521d6c42fcf29b02e12f0240';
+    '2891e193737a894ff3db303dcd4f51983d66f590aa3c4f88de480aa56faa44d7';
   const wallet = new ethers.Wallet(privateKey, provider);
 
   const factory = new ethers.ContractFactory(
