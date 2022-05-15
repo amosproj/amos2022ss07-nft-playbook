7e277bb6b4b0f16824700e1caef671d94f8ec6a1
schlingling <schilling.jn@icloud.com>
2022-05-15 11:43:53 +0200

    added vscoderules
    Co-authored-by: MPSebastian <MPSebastian@users.noreply.github.com>
    Co-authored-by: phildree <phildree@users.noreply.github.com>

--- packages/nft-playbook/src/app/backend/deploy_contract.ts
+++ packages/nft-playbook/src/app/backend/deploy_contract.ts
@@ -7,12 +7,12 @@ export const deploy_contract = async () => {
     './packages/nft-playbook/src/app/backend/contracts/ERC721PresetMinterPauserAutoId.sol'
   );
   console.log(contractInfo['abi']);
-  
+
   const provider = ethers.providers.getDefaultProvider('http://127.0.0.1:7545');
 
   // Use your wallet's private key to deploy the contract, private key of content owner
   const privateKey =
-    '2891e193737a894ff3db303dcd4f51983d66f590aa3c4f88de480aa56faa44d7';
+    '1d7e1d43a7db52ce032125a3a467d118555d0b3d617d10800cc0080e0394b990';
   const wallet = new ethers.Wallet(privateKey, provider);
 
   const factory = new ethers.ContractFactory(
