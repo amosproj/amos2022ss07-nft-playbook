8fca2137cf1a14b3109aedddbbd49e1d824c9aec
Sebastian Schwarzmann <sebi.schwarzmann@googlemail.com>
2022-05-27 08:44:37 +0200

    fixed path issues in solidity file. code can be executed.

--- packages/backend/src/lib/contracts/simple_amos_nft_contract.sol
+++ packages/backend/src/lib/contracts/simple_amos_nft_contract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../../../../../../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../../../../../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract simple_amos_nft_contract is ERC721 {
   uint256 public token_counter;
