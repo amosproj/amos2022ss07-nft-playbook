b4cbd44b3d298342a020e87a1f71b3fa286c7eb9
schlingling <schilling.jn@icloud.com>
2022-06-16 16:04:53 +0200

    impl of minting an nft to solana in one big method Co-authored-by: phildree <phildree@users.noreply.github.com> Co-authored-by: MPSebastian <MPSebastian@users.noreply.github.com>

---
+++ packages/backend/src/lib/Solana/Solana.test.ts
@@ -0,0 +1,13 @@
+import { Solana } from './Solana';
+
+describe('Solana', () => {
+  jest.setTimeout(10000);
+
+  it('should mint NFT to Solana', async () => {
+    console.log('hallo');
+    const solana = new Solana();
+    const test = await solana.test_minting_to_solana();
+    console.log(test);
+    expect(1 == 1);
+  });
+});
--- packages/backend/src/lib/Solana/Solana.ts
+++ packages/backend/src/lib/Solana/Solana.ts
@@ -5,6 +5,22 @@ import { BlockchainConfigReadSmartContract } from '../BlockchainConfig/Blockchai
 import { BlockChainConfigReadTokenData } from '../BlockchainConfig/BlockChainConfigReadTokenData';
 import { BlockChainConfigReadUserDataFromSmartContract } from '../BlockchainConfig/BlockChainConfigReadUserDataFromSmartContract';
 
+import {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+} from '@solana/web3.js';
+import {
+  createMint,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+  setAuthority,
+  transfer,
+} from '@solana/spl-token';
+import { connect } from 'http2';
+
 export class Solana implements Blockchain {
   deploy_contract(config: BlockchainConfigDeployContract): Promise<string> {
     throw new Error('Method not implemented.');
@@ -29,5 +45,87 @@ export class Solana implements Blockchain {
     config: BlockChainConfigReadTokenData
   ): Promise<void> {
     throw new Error('Method not implemented.');
+  }
+
+  //Tutorial from https://www.quicknode.com/guides/web3-sdks/how-to-mint-an-nft-on-solana
+  //Usefull solana terminology https://docs.solana.com/de/terminology
+  async test_minting_to_solana() {
+    // Connect to dev-cluster --> Solana provides online devnet
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+
+    //Generate a new wallet keypair and airdrop SOL --> airdropping is a mechanism in solana dev-net which provides free SOL tokens for testing reasons
+    const fromWallet = Keypair.generate(); //Keypair: A public key and corresponding private key for accessing an account.
+
+    const fromAirDropSignature = await connection.requestAirdrop(
+      fromWallet.publicKey,
+      LAMPORTS_PER_SOL //lamport: A fractional native token with the value of 0.000000001 sol.
+    );
+    console.log('hallofrom solana.ts');
+
+    //Wait for airdrop confirmation
+    await connection.confirmTransaction(fromAirDropSignature);
+
+    //Creating a new token called mint
+    const mint = await createMint(
+      connection,
+      fromWallet, //payer of the tx
+      fromWallet.publicKey, //Account that will control the minting
+      null, //Account that will control the freezing of the token
+      0 //Location of the decimal place
+    ); //more information on https://solana-labs.github.io/solana-program-library/token/js/modules.html
+
+    // Get the token account of the "fromWallet" Solana address. If it does not exist, create it.
+    // You can think about the chain of custody like this: the NFT resides in the account, and your wallet owns this account.
+
+    const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      fromWallet,
+      mint,
+      fromWallet.publicKey
+    );
+
+    //We have an account to send the NFT from; now, we need an account to send the NFT to.
+    //Generate a new wallte to receive the newly minted token
+    const toWallet = Keypair.generate();
+
+    //Get the token account of the toWallet solana adress. If it does not exist, create a new one.
+    const toTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      fromWallet,
+      mint,
+      toWallet.publicKey
+    );
+
+    // Minting 1 new token to the "fromTokenAccount" account we just returned/created.
+    let signature = await mintTo(
+      connection,
+      fromWallet, // Payer of the transaction fees
+      mint, // Mint for the account
+      fromTokenAccount.address, // Address of the account to mint to
+      fromWallet.publicKey, // Minting authority
+      1 // Amount to mint
+    );
+
+    await setAuthority(
+      connection,
+      fromWallet, // Payer of the transaction fees
+      mint, // Account
+      fromWallet.publicKey, // Current authority
+      0, // Authority type: "0" represents Mint Tokens
+      null // Setting the new Authority to null
+    );
+
+    signature = await transfer(
+      connection,
+      fromWallet, // Payer of the transaction fees
+      fromTokenAccount.address, // Source account
+      toTokenAccount.address, // Destination account
+      fromWallet.publicKey, // Owner of the source account
+      1 // Number of tokens to transfer
+    );
+
+    console.log('SIGNATURE', signature);
+    return signature;
+    //Find the minted NFT here: https://explorer.solana.com/?cluster=devnet
   }
 }
